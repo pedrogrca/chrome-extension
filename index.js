64db9fa3e57b282804e1a1cf80d5c8729bda3479
Pedro Henrique Graça <68236129+pedrogrca@users.noreply.github.com>
2025-04-14 09:55:45 -0300

    Added Chrome API support, to get the current tab.

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 let myLeads = [];
 
+const tabs = [
+    {url: "https://www.linkedin.com/in/per-harald-borgen/"}
+]
+
 const inputEl = document.getElementById("input-el");
+const saveBtn = document.getElementById("save-btn")
 const inputBtn = document.getElementById("input-btn");
 const deleteBtn = document.getElementById("delete-btn")
 const ulEl = document.getElementById("ul-el");
@@ -10,34 +15,31 @@ const leadsFromLocalStorage = JSON.parse(localStorage.getItem("myLeads"));
 
 if (leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage;
-    renderLeads();
+    render(myLeads);
 }
+
+saveBtn.addEventListener("click", function(){    
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs){
+        myLeads.push(tabs[0].url)
+        localStorage.setItem("myLeads", JSON.stringify(myLeads));
+        render(myLeads);
+    })
+})
 
 deleteBtn.addEventListener("dblclick", function() {
     localStorage.clear();
     myLeads = [];
-    renderLeads();
-})
-
-inputBtn.addEventListener("click", function() {
-    myLeads.push(inputEl.value);
-    inputEl.value = "";
-    localStorage.setItem("myLeads", JSON.stringify(myLeads));
-
-    renderLeads();
-    console.log( localStorage.getItem("myLeads") );
-
+    render(myLeads);
 })
 
 
-
-function renderLeads() {
+function render(leads) {
     let listItems = "";
-    for(let i = 0; i < myLeads.length; i++){
+    for(let i = 0; i < leads.length; i++){
         listItems += `
         <li>
-            <a target='_blank' href='${myLeads[i]}'>
-                ${myLeads[i]}
+            <a target='_blank' href='${leads[i]}'>
+                ${leads[i]}
             </a>
         </li>
     `;
@@ -46,4 +48,14 @@ function renderLeads() {
     ulEl.innerHTML = listItems;
 
 }
+
+
+
+inputBtn.addEventListener("click", function() {
+    myLeads.push(inputEl.value);
+    inputEl.value = "";
+    localStorage.setItem("myLeads", JSON.stringify(myLeads));
+    render(myLeads);
+})
+
 
